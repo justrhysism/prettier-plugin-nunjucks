17d7075802a93fa6a5b60a9cd37fda264c46e2cc
Rhys Lloyd <justrhysism@gmail.com>
2019-12-22 14:37:45 +1030

    Dependency upgrades

---
+++ build/rollup.config.js
@@ -0,0 +1,63 @@
+import { resolve } from "path";
+
+import nodeResolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import alias from "rollup-plugin-alias";
+import inject from "rollup-plugin-inject";
+import replace from "rollup-plugin-replace";
+import babel from "rollup-plugin-babel";
+import json from "rollup-plugin-json";
+import { terser } from "rollup-plugin-terser";
+
+const SRC_DIR = resolve(__dirname, "..", "src");
+const BUILD_DIR = resolve(__dirname, "..", "build");
+
+export default {
+  input: resolve(SRC_DIR, "index.js"),
+  output: {
+    file: "standalone.js",
+    format: "umd",
+    name: "prettierPlugins.nunjucks",
+    exports: "named",
+    globals: {
+      prettier: "prettier"
+    },
+    paths: {
+      prettier: "prettier/standalone"
+    }
+  },
+  external: ["prettier"],
+  plugins: [
+    nodeResolve({
+      preferBuiltins: true
+    }),
+    commonjs(),
+    alias({
+      entries: [
+        { find: "assert", replacement: resolve(BUILD_DIR, "shims/assert.js") }
+      ]
+    }),
+    inject({
+      Buffer: resolve(BUILD_DIR, "shims/buffer.js")
+    }),
+    replace({
+      "process.arch": JSON.stringify("x32")
+    }),
+    json(),
+    babel({
+      babelrc: false,
+      plugins: [],
+      compact: false,
+      presets: [
+        [
+          require.resolve("@babel/preset-env"),
+          {
+            targets: { browsers: [">0.25%", "not ie 11", "not op_mini all"] },
+            modules: false
+          }
+        ]
+      ]
+    }),
+    terser()
+  ]
+};
--- jest.config.js
+++ jest.config.js
@@ -11,17 +11,34 @@ module.exports = {
   ],
   projects: [
     {
-      displayName: "test",
+      displayName: "test-node",
       setupFiles: ["<rootDir>/tests_config/run_spec.js"],
       testRegex: "jsfmt\\.spec\\.js$|__tests__/.*\\.js$",
-      snapshotSerializers: ["<rootDir>/tests_config/raw-serializer.js"],
-      testEnvironment: "node"
+      snapshotSerializers: ["jest-snapshot-serializer-raw"],
+      testEnvironment: "node",
+      globals: {
+        STANDALONE: false
+      }
     },
-    {
-      runner: "jest-runner-eslint",
-      displayName: "lint",
-      testMatch: ["<rootDir>/**/*.js"],
-      testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/coverage/"]
-    }
+    ...(process.env.SKIP_STANDALONE_TESTS === "true"
+      ? []
+      : [
+        {
+          displayName: "test-standalone",
+          setupFiles: ["<rootDir>/tests_config/run_spec.js"],
+          testRegex: "jsfmt\\.spec\\.js$|__tests__/.*\\.js$",
+          snapshotSerializers: ["jest-snapshot-serializer-raw"],
+          testEnvironment: "jsdom",
+          globals: {
+            STANDALONE: true
+          }
+        }
+      ]),
+    // {
+    //   runner: "jest-runner-eslint",
+    //   displayName: "lint",
+    //   testMatch: ["<rootDir>/**/*.js"],
+    //   testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/coverage/"]
+    // }
   ]
 };
